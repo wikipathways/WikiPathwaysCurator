45a7b586591bd544271062e1f097d27dbb81bab9
De <denise.slenter@maastrichtuniversity.nl>
2018-05-01 13:39:14 +0200

    Fix #1
    Added file with sec,primary IDs from HMDB, added test for WPs Jenkins.

---
+++ src/test/java/nl/unimaas/bigcat/wikipathways/curator/HMDBSecMetabolites.java
@@ -0,0 +1,67 @@
++/* Copyright (C) 2018  Egon Willighagen <egon.willighagen@gmail.com>
++ *
++ * All rights reserved.
++ * 
++ * Redistribution and use in source and binary forms, with or without
++ * modification, are permitted provided that the following conditions are met:
++ *   - Redistributions of source code must retain the above copyright
++ *     notice, this list of conditions and the following disclaimer.
++ *   - Redistributions in binary form must reproduce the above copyright
++ *     notice, this list of conditions and the following disclaimer in the
++ *     documentation and/or other materials provided with the distribution.
++ *   - Neither the name of the <organization> nor the
++ *     names of its contributors may be used to endorse or promote products
++ *     derived from this software without specific prior written permission.
++ * 
++ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
++ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
++ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
++ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
++ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
++ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
++ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
++ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
++ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
++ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
++ */
++package nl.unimaas.bigcat.wikipathways.curator;
++
++import java.util.HashMap;
++import java.util.Map;
++
++import org.apache.jena.rdf.model.Model;
++import org.junit.Assert;
++import org.junit.BeforeClass;
++import org.junit.Test;
++
++public class HMDBMetabolites {
++
++	private static Map<String,String> oldToNew = new HashMap<String, String>();
++
++	@BeforeClass
++	public static void loadData() throws InterruptedException {
++		OPSWPRDFFiles.loadData();
++		Model data = OPSWPRDFFiles.loadData();
++		Assert.assertTrue(data.size() > 5000);
++
++		// now load the deprecation data
++		String deprecatedData = ResourceHelper.resourceAsString("metabolite/hmdb/HMDBDataSecondaryAll_Final.csv");
++		String lines[] = deprecatedData.split("\\r?\\n");
++		for (int i=0; i<lines.length; i++) {
++			String[] ids = lines[i].split(",");
++			oldToNew.put(ids[0], ids[1]);
++		}
++		System.out.println("size: " + oldToNew.size());
++	}
++
++	@Test(timeout=20000)
+	public void outdatedIdentifiers() throws Exception {
+		String sparql = ResourceHelper.resourceAsString("metabolite/hmdb/outdatedHMDBidentifiers.rq");
+		Assert.assertNotNull(sparql);
+		StringMatrix table = SPARQLHelper.sparql(OPSWPRDFFiles.loadData(), sparql);
+		Assert.assertNotNull(table);
+		Assert.assertEquals("Outdated HMDB identifiers:\n" + table, 0, table.getRowCount());
+	}
++	
++
++}
