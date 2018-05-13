905eeee2c770eebcc8a1f82f682864d57f96bfd3
Egon Willighagen <egon.willighagen@gmail.com>
2018-05-13 19:11:02 +0200

    Removed debug info

--- src/test/java/nl/unimaas/bigcat/wikipathways/curator/HMDBSecMetabolites.java
+++ src/test/java/nl/unimaas/bigcat/wikipathways/curator/HMDBSecMetabolites.java
@@ -71,7 +71,6 @@ public class HMDBSecMetabolites {
 		if (table.getRowCount() > 0) {
 			for (int i=1; i<=table.getRowCount(); i++) {
 				String identifier = table.get(i, "identifier");
-				System.out.println("Testing " + identifier);
 				if (oldToNew.containsKey(identifier)) {
 					errors += table.get(i, "homepage") + " " + table.get(i, "label").replace('\n', ' ') +
 						" has " + identifier + " but has primary identifier " +
