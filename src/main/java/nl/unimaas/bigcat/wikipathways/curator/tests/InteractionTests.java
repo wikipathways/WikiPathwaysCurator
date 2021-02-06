8004bef96f84d0dd6f34d34b1500dee416d79088
Egon Willighagen <egon.willighagen@gmail.com>
2021-02-06 16:14:24 +0100

    Updated the test for httpS migration for identifiers.org

--- src/main/java/nl/unimaas/bigcat/wikipathways/curator/tests/InteractionTests.java
+++ src/main/java/nl/unimaas/bigcat/wikipathways/curator/tests/InteractionTests.java
@@ -61,8 +61,8 @@ public class InteractionTests {
 		StringMatrix table = helper.sparql(sparql);
 		assertions.add(new AssertNotNull("InteractionTests", "noMetaboliteToNonMetaboliteConversions", table));
 		Set<String> allowedProteinProducts = new HashSet<String>();
-		allowedProteinProducts.add("http://identifiers.org/uniprot/H9ZYJ2"); // theoredoxin, e.g. WP3580
-		allowedProteinProducts.add("http://identifiers.org/chebi/CHEBI:39026"); // LPL
+            InteractionTests.addIdentifiersOrg(allowedProteinProducts, "identifiers.org/uniprot/H9ZYJ2"); // theoredoxin, e.g. WP3580
+            InteractionTests.addIdentifiersOrg(allowedProteinProducts, "identifiers.org/chebi/CHEBI:39026"); // LPL
 		String errors = "";
 		int errorCount = 0;
 		if (table.getRowCount() > 0) {
