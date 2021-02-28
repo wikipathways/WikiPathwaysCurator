209b6eae4f3639b7902c7555adf75e02d75ae41b
Egon Willighagen <egon.willighagen@gmail.com>
2021-02-28 09:49:29 +0100

    Removed a redundant import

--- src/test/java/nl/unimaas/bigcat/wikipathways/curator/PubChemMetabolites.java
+++ src/test/java/nl/unimaas/bigcat/wikipathways/curator/PubChemMetabolites.java
@@ -31,7 +31,6 @@ import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import nl.unimaas.bigcat.wikipathways.curator.assertions.IAssertion;
