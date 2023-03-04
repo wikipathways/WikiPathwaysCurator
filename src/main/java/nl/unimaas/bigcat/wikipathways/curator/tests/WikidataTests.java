73dbf885b80f542a8cde571332bef865f3254480
Egon Willighagen <egon.willighagen@gmail.com>
2023-03-04 17:29:06 +0100

    Ignore a few classes of glycosphingolipids for now

--- src/main/java/nl/unimaas/bigcat/wikipathways/curator/tests/WikidataTests.java
+++ src/main/java/nl/unimaas/bigcat/wikipathways/curator/tests/WikidataTests.java
@@ -395,8 +395,13 @@ public class WikidataTests {
 		String errors = "";
 		if (table.getRowCount() > 0) {
 			for (int i=1; i<=table.getRowCount(); i++) {
-				errors += table.get(i, "metabolite") + " (" + table.get(i, "label") + ") "
+				String lmid = table.get(i, "metabolite");
+				if (lmid.startsWith("LMSP0502") || lmid.startsWith("LMSP0506") || lmid.startsWith("LMSP0601") || lmid.startsWith("LMSP0602")) {
+					// ignore a few glycosphingolipids for now
+				} else {
+				    errors +=  lmid + " (" + table.get(i, "label") + ") "
 					    + "does not have a Wikidata mapping in " + table.get(i, "homepage") + " ; \n";
+				}
 			}
 		}
 		assertions.add(new AssertEquals(test,
